90ea66e8cf57787b8aa02d9090eda88c5c401fe4
Umut Özel <umutozel@gmail.com>
2017-05-31 16:06:54 +0300

    Cosmetics

--- Giver/ValueGenerators/DateTimeGenerator.cs
+++ Giver/ValueGenerators/DateTimeGenerator.cs
@@ -9,10 +9,11 @@ namespace Giver.ValueGenerators {
         public override DateTime GetValue(MemberInfo memberInfo) {
             var start = DateTime.MinValue;
             var range = DateTime.MaxValue - DateTime.MinValue;
-            var date = start.AddHours(Random.Next(0, (int)range.TotalHours));
-            date = date.AddMinutes(Random.Next(0, 60));
-            date = date.AddSeconds(Random.Next(0, 60));
-            return date.AddMilliseconds(Random.Next(0, 1000));
+            return start
+                .AddHours(Random.Next(0, (int)range.TotalHours))
+                .AddMinutes(Random.Next(0, 60))
+                .AddSeconds(Random.Next(0, 60))
+                .AddMilliseconds(Random.Next(0, 1000));
         }
     }
 }
